645b6314520ebd824490575e351cbd032f180adb
maxgotech <golovinmd@mail.ru>
2023-11-07 13:07:04 +0300

    Файлы сервиса

---
+++ src/app/shared/data-access/markup/markup.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarkupService {
+
+  constructor(private readonly http: HttpClient) { }
+
+  log(mail:string,password:string){
+    return this.http.post('/api/user/pfpload', {mail,password} );
+  }
+
+}
