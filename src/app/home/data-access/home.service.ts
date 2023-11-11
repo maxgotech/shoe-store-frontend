a3cd39be7d8af2ed0515da123273e7bb0072bb40
maxgotech <golovinmd@mail.ru>
2023-11-11 22:36:18 +0300

    Добавлено кэширование

--- src/app/home/data-access/home.service.ts
+++ src/app/home/data-access/home.service.ts
@@ -1,16 +1,35 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable, timer } from 'rxjs';
+import { switchMap, shareReplay  } from 'rxjs/operators';
+const REFRESH_INTERVAL = 10000;
+const CACHE_SIZE = 1;
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
   constructor(private readonly http: HttpClient) { }
 
-  GetProducts(){
+  private cache$!: Observable<Array<any>>;
+  get products() {
+    if (!this.cache$) {
+      // Set up timer that ticks every X milliseconds
+      const timer$ = timer(0, REFRESH_INTERVAL);
+
+      // For each tick make an http request to fetch new data
+      console.log('new data triggered')
+      this.cache$ = timer$.pipe(
+        switchMap(_ => this.requestProducts()),
+        shareReplay(CACHE_SIZE)
+      );
+    }
+    return this.cache$;
+  }
+
+  private requestProducts() {
     let queryParams = new HttpParams();
     queryParams = queryParams.append("type",'shoes');
-    return this.http.get('/purchaseApi/api/products',{params:queryParams});
+    return this.http.get<any>('/purchaseApi/api/products',{params:queryParams});
   }
 
 }
--- src/app/home/feature/home.component.ts
+++ src/app/home/feature/home.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../data-access/home.service';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, take } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(private homeService:HomeService){
+  }
+
+  ngOnInit(): void {
     this.GetData()
   }
 
   products:any
 
   async GetData(){
-    const data = this.homeService.GetProducts()
+    const data = this.homeService.products.pipe(take(1))
     const response = await lastValueFrom(data)
-    this.products = response
-    console.log(response)
+    this.products=response
   }
 
 }
