f32485d2552be78c6053bda67a627555cae19b15
maxgotech <golovinmd@mail.ru>
2023-11-12 17:43:41 +0300

    Карточки товаров

--- src/app/home/ui/productCards/product-cards.component.ts
+++ src/app/home/ui/productCards/product-cards.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-product-cards',
   templateUrl: './product-cards.component.html',
-  styleUrls: ['./product-cards.component.less']
+  styleUrls: ['./product-cards.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardsComponent {
+export class ProductCardsComponent implements OnChanges {
   @Input() products:any
   constructor(){
 
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['products'].currentValue!=null){
+      this.loaded=true
+      console.log(this.products)
+    }
+  }
+
+  loaded:boolean = false
 
 }
