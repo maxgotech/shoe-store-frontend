69a4516e1f297851850b1e4dc276e8df5f285a34
maxgotech <golovinmd@mail.ru>
2023-11-12 20:21:26 +0300

    добавлена функция логина в сервис

--- src/app/shared/data-access/markup/markup.service.ts
+++ src/app/shared/data-access/markup/markup.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +8,7 @@ export class MarkupService {
   constructor(private readonly http: HttpClient) { }
 
   log(mail:string,password:string){
-    return this.http.post('/api/user/pfpload', {mail,password} );
+    return this.http.post('/markupApi/auth/log', {mail,password} );
   }
 
 }
