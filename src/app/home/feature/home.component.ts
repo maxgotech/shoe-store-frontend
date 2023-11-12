092b52ccb4ef060537449ff7bd3d1dfa7b207838
maxgotech <golovinmd@mail.ru>
2023-11-12 23:43:40 +0300

    дата биндинг логина и логаута с хэдером

--- src/app/home/feature/home.component.ts
+++ src/app/home/feature/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../data-access/home.service';
 import { lastValueFrom, take } from 'rxjs';
+import { AuthService } from 'src/app/shared/data-access/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,19 +9,28 @@ import { lastValueFrom, take } from 'rxjs';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  constructor(private homeService:HomeService){
+  constructor(private homeService:HomeService,private authService:AuthService){
   }
 
   ngOnInit(): void {
+    this.loggedIn = !!this.authService.currentUserValue;
     this.GetData()
   }
 
+  loggedIn:boolean = false
   products:any
 
   async GetData(){
     const data = this.homeService.products.pipe(take(1))
     const response = await lastValueFrom(data)
     this.products=response
+  }
+
+  logout(flag:boolean){
+    if(flag==true){
+      this.authService.logout()
+      window.location.reload();
+    }
   }
 
 }
