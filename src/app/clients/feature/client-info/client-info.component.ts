a3852dc008436bf116bbf89f17617bfc63277891
maxgotech <golovinmd@mail.ru>
2023-11-19 18:36:37 +0300

    добавлен navbar на страницу

--- src/app/clients/feature/client-info/client-info.component.ts
+++ src/app/clients/feature/client-info/client-info.component.ts
@@ -4,14 +4,16 @@ import { FooterUiComponent } from '../../../shared/ui/footer-ui/footer-ui.compon
 import { ClientInfoUiComponent } from '../../ui/client-info-ui/client-info-ui.component';
 import { HeaderComponent } from 'src/app/shared/feature/header/header.component';
 import { ClientsService } from '../../data-access/clients.service';
+import { NavBarUiComponent } from '../../ui/nav-bar-ui/nav-bar-ui.component';
 @Component({
-    templateUrl: './client-info.component.html',
-    styleUrls: ['./client-info.component.less'],
-    standalone: true,
-    imports: [HeaderComponent, ClientInfoUiComponent, FooterUiComponent]
+  templateUrl: './client-info.component.html',
+  styleUrls: ['./client-info.component.less'],
+  standalone: true,
+  imports: [HeaderComponent, ClientInfoUiComponent, FooterUiComponent,NavBarUiComponent]
 })
 export class ClientInfoComponent {
-  constructor(private router: Router,private clientsService:ClientsService){
+  constructor(private router: Router, private clientsService: ClientsService) {
+    console.log(this.clientsService.ClientInfo())
   }
 
 }
