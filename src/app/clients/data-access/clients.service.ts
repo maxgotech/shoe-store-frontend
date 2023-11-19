2f10e4c62196c0822656c3d96aa7a324732cdca8
maxgotech <golovinmd@mail.ru>
2023-11-20 00:15:45 +0300

    функция возврата данных пользователя

--- src/app/clients/data-access/clients.service.ts
+++ src/app/clients/data-access/clients.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from 'src/app/shared/data-access/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
 
-  constructor() { }
+  constructor(private readonly http: HttpClient, private authService: AuthService) { }
+
+  ClientInfo() {
+    return this.authService.currentUserValue
+  }
+
 }
