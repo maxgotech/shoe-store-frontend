fb25e3ebbb94a095bd2043fb199d18227d5d9a94
maxgotech <golovinmd@mail.ru>
2023-11-20 00:52:02 +0300

    добавлены все роуты клиента

--- src/app/clients/feature/client-routes/client-routes.component.ts
+++ src/app/clients/feature/client-routes/client-routes.component.ts
@@ -1,14 +1,29 @@
 import { ClientsService } from "../../data-access/clients.service";
+import { ClientCartComponent } from "../client-cart/client-cart.component";
 import { ClientInfoComponent } from "../client-info/client-info.component";
+import { ClientProductsComponent } from "../client-products/client-products.component";
+import { ClientSalesComponent } from "../client-sales/client-sales.component";
 
 export default [
   {
     path: '',
-    providers:[ClientsService],
+    providers: [ClientsService],
     children: [
       {
         path:'info',
-        component:ClientInfoComponent
+        component: ClientInfoComponent
+      },
+      {
+        path: 'cart',
+        component: ClientCartComponent
+      },
+      {
+        path: 'products',
+        component: ClientProductsComponent
+      },
+      {
+        path: 'sales',
+        component: ClientSalesComponent
       }
     ]
   }
