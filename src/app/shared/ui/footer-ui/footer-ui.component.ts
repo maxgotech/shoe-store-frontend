410e5a45888c404db5486161cc0385e92da79c22
maxgotech <golovinmd@mail.ru>
2023-11-19 15:42:13 +0300

    footer переименован в footer-ui

---
+++ src/app/shared/ui/footer-ui/footer-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-footer-ui',
+    templateUrl: './footer-ui.component.html',
+    styleUrls: ['./footer-ui.component.less'],
+    standalone: true
+})
+export class FooterUiComponent {
+
+}
