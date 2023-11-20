e23b4915897b647fad7b92e7e76f5a3fc223b46f
maxgotech <golovinmd@mail.ru>
2023-11-21 02:08:06 +0300

    ui корзины товаров

--- src/app/clients/ui/client-cart-ui/client-cart-ui.component.ts
+++ src/app/clients/ui/client-cart-ui/client-cart-ui.component.ts
@@ -1,14 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { TuiLoaderModule } from '@taiga-ui/core';
+
+interface productdto {
+  products?:product[]
+  empty:boolean
+}
+
+interface product {
+  id:number
+  name:string
+  price:number
+  picture_path:string
+}
 
 @Component({
   selector: 'app-client-cart-ui',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, TuiLoaderModule],
   templateUrl: './client-cart-ui.component.html',
   styleUrls: ['./client-cart-ui.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClientCartUiComponent { }
+export class ClientCartUiComponent implements OnChanges { 
+
+  @Input() productDto!:productdto
+
+  cartStatus:boolean = true
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['productDto'].currentValue!=undefined){
+      this.loading=false
+      if(this.productDto.empty==true){
+        this.cartStatus=false
+      }
+    }
+  }
+
+  loading:boolean = true
+
+}
