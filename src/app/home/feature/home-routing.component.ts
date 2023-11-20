4d6c95da8075488aeb09ee5dd52b64289a495a59
maxgotech <golovinmd@mail.ru>
2023-11-20 13:59:05 +0300

    роут регистрации

--- src/app/home/feature/home-routing.component.ts
+++ src/app/home/feature/home-routing.component.ts
@@ -1,12 +1,14 @@
 import { LoginComponent } from "src/app/shared/feature/login/login.component";
 import { HomeComponent } from "./home.component";
 import { GenerateDialogableRoute } from "src/app/utils/RouteDialog/GeneratedDialogableRoute.component";
+import { RegComponent } from "src/app/shared/feature/reg/reg.component";
 export default [
   {
     path: '',
     component: HomeComponent,
     children: [
-      GenerateDialogableRoute(LoginComponent,'login')
+      GenerateDialogableRoute(LoginComponent,'login'),
+      GenerateDialogableRoute(RegComponent,'reg'),
     ]
   }
 ]
