018da90a92a255a2b66bab7da8f73bc306f2fbad
maxgotech <golovinmd@mail.ru>
2023-11-21 02:09:06 +0300

    передача данных в ui корзины

--- src/app/clients/feature/client-cart/client-cart.component.ts
+++ src/app/clients/feature/client-cart/client-cart.component.ts
@@ -1,9 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ClientCartUiComponent } from '../../ui/client-cart-ui/client-cart-ui.component';
 import { HeaderComponent } from 'src/app/shared/feature/header/header.component';
 import { FooterUiComponent } from 'src/app/shared/ui/footer-ui/footer-ui.component';
 import { NavBarUiComponent } from '../../ui/nav-bar-ui/nav-bar-ui.component';
+import { AuthService } from 'src/app/shared/data-access/auth.service';
+import { ClientsService } from '../../data-access/clients.service';
+import { map } from 'rxjs';
+
+interface productdto {
+  products?:product[]
+  empty:boolean
+}
+
+interface product {
+  id:number
+  name:string
+  price:number
+  picture_path:string
+}
 
 @Component({
   selector: 'app-client-cart',
@@ -19,4 +34,35 @@ import { NavBarUiComponent } from '../../ui/nav-bar-ui/nav-bar-ui.component';
   styleUrls: ['./client-cart.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClientCartComponent { }
+export class ClientCartComponent implements OnInit { 
+  constructor(private authService:AuthService, private clientService:ClientsService, private cdr: ChangeDetectorRef){}
+  
+  ngOnInit(): void {
+    this.FindCart()
+  }
+
+  user = this.authService.currentUserValue
+  productDto!:productdto
+  products:product[] = []
+
+  async FindCart(){
+    this.clientService.findCart(this.user.data.id).pipe(map((carts)=>{
+      carts.forEach(element => {
+        this.products = this.products!.concat(element.product)
+      });
+    })).subscribe((data)=>{
+      if(this.products.length==0){
+        this.productDto = {
+          empty:true
+        }
+      } else
+      {
+        this.productDto = {
+          products:this.products,
+          empty: false
+        }
+      }
+      this.cdr.detectChanges()
+    })
+  }
+}
