b9b63820eecbbdaaab53e160c188aed4afc32177
maxgotech <golovinmd@mail.ru>
2023-11-26 20:39:44 +0300

    ф-и покупки товаров и удаления товаров из корзины

--- src/app/clients/data-access/clients.service.ts
+++ src/app/clients/data-access/clients.service.ts
@@ -13,6 +13,17 @@ interface product {
   picture_path:string
 }
 
+interface buyProductDto {
+  quantity:number
+  markup:number
+  productId:prodIds
+  userId:number
+}
+
+interface prodIds {
+  id:number
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -28,6 +39,14 @@ export class ClientsService {
     let queryParams = new HttpParams();
     queryParams = queryParams.append("id",userId);
     return this.http.get<cart[]>('purchaseApi/api/cart',{params:queryParams})
+  }
+
+  buyProducts(bought_prod:buyProductDto){
+    return this.http.post<any>('purchaseApi/api/purchase',{quantity:bought_prod.quantity, markup:bought_prod.markup, productId:bought_prod.productId, userId:bought_prod.userId})
+  }
+
+  deleteProdFromCart(id:number){
+    return this.http.delete<any>('purchaseApi/api/cart/'+id)
   }
 
 }
