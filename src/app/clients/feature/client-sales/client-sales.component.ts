7425638a346e7ad0ba9422e64d819fc077501ae2
maxgotech <golovinmd@mail.ru>
2023-11-27 23:26:16 +0300

    smart компонент страницы продаж

--- src/app/clients/feature/client-sales/client-sales.component.ts
+++ src/app/clients/feature/client-sales/client-sales.component.ts
@@ -1,9 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HeaderComponent } from 'src/app/shared/feature/header/header.component';
 import { FooterUiComponent } from 'src/app/shared/ui/footer-ui/footer-ui.component';
 import { NavBarUiComponent } from '../../ui/nav-bar-ui/nav-bar-ui.component';
 import { ClientSalesUiComponent } from '../../ui/client-sales-ui/client-sales-ui.component';
+import { ClientsService } from '../../data-access/clients.service';
+import { lastValueFrom, map } from 'rxjs';
+
+interface SalesInfo {
+  count_mounth:number,
+  all_products:SalesProd[]
+}
+
+interface SalesProd {
+  product:string
+  price:number
+}
 
 @Component({
   selector: 'app-client-sales',
@@ -19,4 +31,45 @@ import { ClientSalesUiComponent } from '../../ui/client-sales-ui/client-sales-ui
   styleUrls: ['./client-sales.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClientSalesComponent { }
+export class ClientSalesComponent implements OnInit { 
+  constructor(private clientService:ClientsService, private cdr: ChangeDetectorRef){}
+
+  user = this.clientService.ClientInfo()
+
+  products:any
+  sales:any
+
+  ngOnInit(): void {
+  }
+
+  getProds(){
+    return this.clientService.allProducts(this.user.data.id).pipe(map((prods) => {
+      prods.forEach((prod: any) => {
+        const price_with_markup = prod.product.price + prod.markup / 100 * prod.product.price;
+        const prodToGen: SalesProd = {
+          product: prod.product.name,
+          price: price_with_markup
+        };
+        this.products = this.products.concat(prodToGen);
+      });
+    }
+    ))
+  }
+
+  async GenSales(month:number){
+    this.products = []
+    const prods_req = this.getProds()
+    const prod_resp = await lastValueFrom(prods_req)
+    const GenObj: SalesInfo = {
+      count_mounth: month,
+      all_products: this.products
+    };
+    const orders_req = this.clientService.genOrders(GenObj)
+    const orders_resp = await lastValueFrom(orders_req)
+    const sales_info_req = this.clientService.salesInfo(orders_resp)
+    const sales_info_resp = await lastValueFrom(sales_info_req)
+    this.sales = sales_info_resp
+    this.cdr.detectChanges()
+  }
+
+}
