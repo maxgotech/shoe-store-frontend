5497d313c99b10d7ae45a2d0d63a1f1bd5c1da9c
maxgotech <golovinmd@mail.ru>
2023-11-28 09:48:01 +0300

    окно  XYZ анализа

---
+++ src/app/clients/ui/dialogs/xyz_analysis/xyz_analysis.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject, TemplateRef } from '@angular/core';
+import { TuiDialogContext, TuiDialogService } from '@taiga-ui/core';
+import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
+import { TuiTableModule } from '@taiga-ui/addon-table';
+@Component({
+  selector: 'app-xyz-analysis',
+  standalone: true,
+  imports: [
+    CommonModule,
+    TuiTableModule
+  ],
+  templateUrl: './xyz_analysis.component.html',
+  styleUrl: './xyz_analysis.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class XyzAnalysisComponent {
+  constructor(@Inject(TuiDialogService) private readonly dialogs: TuiDialogService,
+  @Inject(POLYMORPHEUS_CONTEXT) private readonly context: TuiDialogContext<number, number>) { 
+    this.data=this.getData
+    console.log(this.data)
+  }
+
+  showDialog(content: TemplateRef<TuiDialogContext>): void {
+    this.dialogs.open(content, { dismissible: true }).subscribe();
+  }
+
+  data:any
+
+  get getData(): number {
+    return this.context.data;
+  }
+
+  readonly columns = ['prod','xyz','coef_var']
+
+}
